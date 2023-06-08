cfaa498a51e596e44c3ed7e3aa3f6567655eed5b
Michael Goldstein <mlgoldstein28@gmail.com>
2023-06-08 10:10:08 -0400

    Mobile Responsive

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -10,15 +10,15 @@ const Cards = ({meals, lastIndex}) => {
     //Pagination
       
       return ( 
-        <div key={idMeal} className="col-lg-3 col-md-4 col-sm-4 col-12">
+        <div key={idMeal} className="col-lg-3 col-md-3 col-sm-4 col-12">
           <Link to={`${idMeal}`}
                 key={idMeal}
                 style={{textDecoration: 'none'}}
-                className="col-lg-3 col-md-4 col-sm-4 col-12 text-dark">
+                className="col-lg-3 col-md-5 col-sm-6 col-12 text-dark">
             <div className="d-flex flex-column justify-content">
               <div className={`${styles.card} mb-3`}>
                 <img className={`${styles.img} img-fluid mb-2`} src={strMealThumb} alt="" />
-                <div className="fs-6 fw-bold m-3 mt-0 mb-1 d-flex justify-content-start">{strMeal}</div>
+                <div className={`${styles.mealName}`}>{strMeal}</div>
               </div>
             </div>
           </Link>
