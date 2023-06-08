0640069ab8c2b33a0f794efa848d0c217a4f37c9
Michael Goldstein <mlgoldstein28@gmail.com>
2023-06-08 10:22:31 -0400

    Mobile Responsive 1.1

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,10 @@
 import {NavLink} from 'react-router-dom';
+import styles from './Navbar.module.scss';
 
 const Navbar = () => {
    return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a className="navbar-brand" href="/"><span className="m-3 fs-3 fw-bold">Recipes </span><span className="fs-6">with MealDB's API</span></a>
+        <a className="navbar-brand" href="/"><span className="m-3 fs-3 fw-bold">Recipes </span><span className={`${styles.subHead}`}>with MealDB's API</span></a>
         <div className="ms-auto">
             <ul className="navbar-nav">
                 <li className="nav-item">
